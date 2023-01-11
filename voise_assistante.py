ce8532fd196bc06588d0b2d5be6ff0325f0110b4
HONOR <saurin70@gmail.com>
2023-01-11 15:04:51 +0300

    initial

--- voise_assistante.py
+++ voise_assistante.py
@@ -4,69 +4,87 @@ import speech_recognition
 import speech_recognition as sr
 import re
 from cflib.positioning.motion_commander import MotionCommander
+import pyaudio
+import cflib
+from cflib.crazyflie import Crazyflie
+from CrazyLogger import CrazyLogger, create_lg_stab
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+from cflib.utils import uri_helper
 
-def voice_commander(scf):
-    if re.findall(r'[А-Яа-яA-Za-z0-9% ,\-\"«»\[\]\']*' + 'Прямо' + r'[А-Яа-яA-Za-z0-9% ,\-\"«»\[\]\']*',
-                  transcript, re.IGNORECASE):
-     forward(scf)
-     voice_commander(scf)
-    elif re.findall(r'[А-Яа-яA-Za-z0-9% ,\-\"«»\[\]\']*' + 'Назад' + r'[А-Яа-яA-Za-z0-9% ,\-\"«»\[\]\']*',
-                  transcript, re.IGNORECASE):
-     back(scf)
-     voice_commander(scf)
-    elif re.findall(r'[А-Яа-яA-Za-z0-9% ,\-\"«»\[\]\']*' + 'Вверх' + r'[А-Яа-яA-Za-z0-9% ,\-\"«»\[\]\']*',
-                  transcript, re.IGNORECASE):
-     up(scf)
-     voice_commander(scf)
-    elif re.findall(r'[А-Яа-яA-Za-z0-9% ,\-\"«»\[\]\']*' + 'Вниз' + r'[А-Яа-яA-Za-z0-9% ,\-\"«»\[\]\']*',
-                  transcript, re.IGNORECASE):
-     down(scf)
-     voice_commander(scf)
-        
+is_flying = True
 
 
 
-def forward():
-    motion_controller = MotionCommander(scf)
-    motion_controller.take_off()
-    motion_controller.forward(1)
-    time.sleep(1)
 
-def back():
-    motion_controller = MotionCommander(scf)
-    motion_controller.take_off()
-    motion_controller.back(1)
-    time.sleep(1)
-
-def up():
-    motion_controller = MotionCommander(scf)
-    motion_controller.take_off()
-    motion_controller.up(1)
-    time.sleep(1)
-
-def down():
-    motion_controller = MotionCommander(scf)
-    motion_controller.take_off()
+def down(motion_controller):
     motion_controller.down(1)
     time.sleep(1)
 
+def forward(motion_controller):
+    motion_controller.forward(1)
+    time.sleep(1)
+
+def back(motion_controller):
+    motion_controller.back(1)
+    time.sleep(1)
+
+def up(motion_controller):
+    motion_controller.up(1)
+    time.sleep(1)
+
+def take_off(motion_controller):
+    motion_controller.take_off()
+    time.sleep(1)
+
+def land(motion_controller):
+    motion_controller.land(0.2)
+    time.sleep(1)
+
+def end_flying(motion_controller):
+    global is_flying
+    land(motion_controller)
+    is_flying = False
+
+command_map = {
+    "взлёт": take_off,
+    "посадка": land,
+    "вперёд" : forward,
+    "назад" : back,
+    "вверх" : up,
+    "вниз" : down,
+    "конец" : end_flying
+}
+
+def voice_commander(motion_commander):
+    global is_flying
+    for command, func in command_map.items():
+        if command in transcript.lower():
+            func(motion_commander)
+
 
 if __name__ == '__main__':
+    uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
     index = 0
+    transcript = ''
     srr = sr.Recognizer()
     mic = sr.Microphone()
-
     cflib.crtp.init_drivers()
-    with SyncCrazyflie(uri, cf=CrazyLogger(create_lg_stab(), rw_cache='./cache')) as scf:
-        voice_commander(scf)
 
-    while True:
-        with mic as sourse:
-            try:
-                srr.energy_threshold = 900
-                srr.adjust_for_ambient_noise(sourse)
-                audio = srr.listen(sourse)
-                transcript = srr.recognize_google(audio, language='ru-RU')
-                print(transcript)
-            except speech_recognition.UnknownValueError:
-                pass
+
+
+
+    with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
+        motion_controller = MotionCommander(scf)
+
+        while is_flying:
+            with mic as sourse:
+                try:
+                    srr.energy_threshold = 900
+                    srr.adjust_for_ambient_noise(sourse)
+                    audio = srr.listen(sourse)
+                    transcript = srr.recognize_google(audio, language='ru-RU')
+                    print(transcript)
+                    voice_commander(motion_controller)
+
+                except speech_recognition.UnknownValueError:
+                    pass
