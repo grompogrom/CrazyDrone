0e5bed06518b5c085a3f7bf5350a9147070b8c96
HONOR <saurin70@gmail.com>
2023-01-18 15:46:56 +0300

    swarm

--- swarm_test.py
+++ swarm_test.py
@@ -1,11 +1,9 @@
 import time
-
 import cflib.crtp
 from cflib.crazyflie import HighLevelCommander
 from cflib.crazyflie.swarm import CachedCfFactory
 from cflib.crazyflie.swarm import Swarm
 
-from CrazyFlieWithPosController import PosControllerCfFactory
 
 
 def activate_led_bit_mask(scf):
@@ -30,10 +28,19 @@ def take_off(scf):
 def land(scf):
     print('land')
     commander: HighLevelCommander= scf.cf.high_level_commander
-
     commander.land(absolute_height_m=0, duration_s=4)
     time.sleep(5)
     commander.stop()
+
+#Function swap position
+def swap_positions(scf):
+  take_off(scf)
+  time.sleep(3)
+
+ #////////////////////////////////
+
+def mask_up(scf):
+
 
 def hover_sequence(scf):
     take_off(scf)
@@ -46,23 +53,28 @@ def hover_sequence(scf):
 
 def change_position(scf):
     commander: HighLevelCommander= scf.cf.high_level_commander
-    commander.go_to(3,0,1,0,3)
+    commander.go_to(1,0,1,0,3)
     time.sleep(3)
 
 def rotate(scf):
     commander: HighLevelCommander= scf.cf.high_level_commander
-    commander.go_to(0,0,0,180,2,True)
+    commander.go_to(0,0,0,90,2,True)
 
 
 uris = {
-    'radio://0/110/2M/E7E7E7E7E3',
+    #'radio://0/110/2M/E7E7E7E7E3',
     'radio://0/110/2M/E7E7E7E7E9',
     'radio://0/80/2M/E7E7E7E7E7',
-    'radio://0/80/2M/E7E7E7E7E8'
+    #'radio://0/80/2M/E7E7E7E7E8'
 
     # 'radio://0/20/2M/E7E7E7E704',
     # Add more URIs if you want more copters in the swarm
 }
+
+group_mask = {
+'radio://0/110/2M/E7E7E7E7E9'
+}
+
 if __name__ == '__main__':
     cflib.crtp.init_drivers()
     factory = CachedCfFactory(rw_cache='./cache')
@@ -71,7 +83,6 @@ if __name__ == '__main__':
         # swarm.parallel_safe(light_check)
         swarm.reset_estimators()
         print('estimators reseted')
-
-        swarm.parallel(hover_sequence)
+        swarm.parallel(swap_positions)
 
 
