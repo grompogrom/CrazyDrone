80b9e4508943b73eadc81d6267f392b0148f313f
magic <gromvmens@gmail.com>
2023-01-21 17:45:06 +0300

    stable

--- Swarm_conf.py
+++ Swarm_conf.py
@@ -1,8 +1,9 @@
 class SwarmConfing:
-    def __init__(self, count, *uris):
+    def __init__(self, *uris):
         self._uris = uris
+        self._count = len(uris)
         self._params = dict()
-        [self._params.update({uris[i]: [False, {'x': 0, 'y': 0, 'z': 0}]}) for i in range(count)]
+        [self._params.update({uris[i]: [False, {'x': 0, 'y': 0, 'z': 0}]}) for i in range(self._count)]
 
     def get_uris(self):
         return set(self._uris)
@@ -15,5 +16,6 @@ class SwarmConfing:
 
 
 if __name__ == '__main__':
-    conf = SwarmConfing(3, 'a', 'b', 'c')
+    conf = SwarmConfing('a', 'b', 'c')
+    conf.add_end_point('a', 1, 1, 1)
     print(conf.get_params())
